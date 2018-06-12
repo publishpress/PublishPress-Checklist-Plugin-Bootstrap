f44a8c823cddbb19f9b78683691cc13ff248451d
Anderson Grudtner Martins <anderson@ostraining.com>
2018-06-11 23:59:05 -0300

    Fix the code style for WordPress style

--- core/Plugin.php
+++ core/Plugin.php
@@ -12,74 +12,69 @@
 
 namespace PublishPress_Checklist_Bootstrap;
 
-defined('ABSPATH') or die('No direct script access allowed.');
+defined( 'ABSPATH' ) or die( 'No direct script access allowed.' );
 
 /**
  * Class Plugin
  *
  * @package PublishPress_Checklist_Bootstrap
  */
-class Plugin
-{
-    /**
-     * Initialize the plugin's class.
-     */
-    public function init()
-    {
-        /**
-         *
-         */
-        add_action('pp_checklist_load_addons', [$this, 'action_load_addons']);
-    }
+class Plugin {
+	/**
+	 * Initialize the plugin's class.
+	 */
+	public function init() {
+		/**
+		 *
+		 */
+		add_action( 'pp_checklist_load_addons', [ $this, 'action_load_addons' ] );
+	}
 
-    /**
-     * Action triggered before load requirements, but after loading the add-ons.
-     * We use this to set the hooks to load our custom filters.
-     */
-    public function action_load_addons()
-    {
-        add_action('pp_checklist_enqueue_scripts', [$this, 'enqueue_admin_scripts']);
-        add_filter('pp_checklist_post_type_requirements', [$this, 'filter_post_type_requirements'], 10, 2);
-    }
+	/**
+	 * Action triggered before load requirements, but after loading the add-ons.
+	 * We use this to set the hooks to load our custom filters.
+	 */
+	public function action_load_addons() {
+		add_action( 'pp_checklist_enqueue_scripts', [ $this, 'enqueue_admin_scripts' ] );
+		add_filter( 'pp_checklist_post_type_requirements', [ $this, 'filter_post_type_requirements' ], 10, 2 );
+	}
 
-    /**
-     * Enqueue scripts and stylesheets for the admin pages.
-     */
-    public function enqueue_admin_scripts()
-    {
-        wp_enqueue_script(
-            'pp-checklist-bootstrap-admin',
-            plugins_url('/assets/js/requirements.js', PP_CHECKLIST_BOOTSTRAP_PLUGIN_FILE),
-            ['jquery', 'pp-checklist-requirements'],
-            PUBLISHPRESS_WOOCOMMERCE_CHECKLIST_VERSION,
-            true
-        );
-    }
+	/**
+	 * Enqueue scripts and stylesheets for the admin pages.
+	 */
+	public function enqueue_admin_scripts() {
+		wp_enqueue_script(
+			'pp-checklist-bootstrap-admin',
+			plugins_url( '/assets/js/requirements.js', PP_CHECKLIST_BOOTSTRAP_PLUGIN_FILE ),
+			[ 'jquery', 'pp-checklist-requirements' ],
+			PUBLISHPRESS_WOOCOMMERCE_CHECKLIST_VERSION,
+			true
+		);
+	}
 
-    /**
-     * Set the requirements list for the given post type
-     *
-     * @param  array  $requirements
-     * @param  string $post_type
-     *
-     * @return array
-     */
-    public function filter_post_type_requirements($requirements, $post_type)
-    {
-        $classes = [];
+	/**
+	 * Set the requirements list for the given post type
+	 *
+	 * @param  array  $requirements
+	 * @param  string $post_type
+	 *
+	 * @return array
+	 */
+	public function filter_post_type_requirements( $requirements, $post_type ) {
+		$classes = [];
 
-        switch ($post_type) {
-            case 'page':
-                $classes = [
-                    '\\PublishPress_Checklist_Bootstrap\\Requirement\\Custom_Text',
-                ];
-                break;
-        }
+		switch ( $post_type ) {
+			case 'page':
+				$classes = [
+					'\\PublishPress_Checklist_Bootstrap\\Requirement\\Custom_Text',
+				];
+				break;
+		}
 
-        if (!empty($classes)) {
-            $requirements = array_merge($requirements, $classes);
-        }
+		if ( ! empty( $classes ) ) {
+			$requirements = array_merge( $requirements, $classes );
+		}
 
-        return $requirements;
-    }
+		return $requirements;
+	}
 }
--- publishpress-checklist-plugin-bootstrap.php
+++ publishpress-checklist-plugin-bootstrap.php
@@ -20,57 +20,58 @@
  * Author URI:  https://publishpress.com
  */
 
-if (!defined('ABSPATH')) {
-    die('No direct script access allowed.');
+if ( ! defined( 'ABSPATH' ) ) {
+	die( 'No direct script access allowed.' );
 }
 
 // Avoid to load it twice.
-if (!defined('PP_CHECKLIST_BOOTSTRAP_LOADED')) {
+if ( ! defined( 'PP_CHECKLIST_BOOTSTRAP_LOADED' ) ) {
 
-    /**
-     * Call the composer's autoload. Avoid to use your own autoload.
-     */
-    $autoloadPath = __DIR__ . '/vendor/autoload.php';
-    if (file_exists($autoloadPath)) {
-        require_once $autoloadPath;
-    }
+	/**
+	 * Call the composer's autoload. We advise to use it for your own libraries.
+	 */
+	$autoloadPath = __DIR__ . '/vendor/autoload.php';
+	if ( file_exists( $autoloadPath ) ) {
+		require_once $autoloadPath;
+	}
 
-    /**
-     * Define the plugin's name.
-     */
-    if (!defined('PP_CHECKLIST_BOOTSTRAP_PLUGIN_NAME')) {
-        define('PP_CHECKLIST_BOOTSTRAP_PLUGIN_NAME', 'PublishPress Checklist Plugin Bootstrap');
-    }
+	/**
+	 * Define the plugin's name.
+	 */
+	if ( ! defined( 'PP_CHECKLIST_BOOTSTRAP_PLUGIN_NAME' ) ) {
+		define( 'PP_CHECKLIST_BOOTSTRAP_PLUGIN_NAME', 'PublishPress Checklist Plugin Bootstrap' );
+	}
 
-    // Define the constant to store the plugins' file path.
-    define('PP_CHECKLIST_BOOTSTRAP_PLUGIN_FILE', 'publishpress-checklist-plugin-bootstrap/publishpress-checklist-plugin-bootstrap.php');
+	// Define the constant to store the plugins' file path.
+	define( 'PP_CHECKLIST_BOOTSTRAP_PLUGIN_FILE',
+		'publishpress-checklist-plugin-bootstrap/publishpress-checklist-plugin-bootstrap.php' );
 
-    /**
-     * Define here the minimum version of PublishPress required by your code.
-     * Use the most recent release when starting developing.
-     */
-    if (!defined('PP_CHECKLIST_BOOTSTRAP_MIN_PARENT_VERSION')) {
-        define('PP_CHECKLIST_BOOTSTRAP_MIN_PARENT_VERSION', '1.12.0');
-    }
+	/**
+	 * Define here the minimum version of PublishPress required by your code.
+	 * Use the most recent release when starting developing.
+	 */
+	if ( ! defined( 'PP_CHECKLIST_BOOTSTRAP_MIN_PARENT_VERSION' ) ) {
+		define( 'PP_CHECKLIST_BOOTSTRAP_MIN_PARENT_VERSION', '1.12.0' );
+	}
 
-    /**
-     * Instantiate the PublishPress Add-on Framework. It is a simple framework
-     * used to check PublishPress requirements. It is a dependency managed using
-     * composer.
-     */
-    $initializer = new PublishPressAddonFramework\Initializer(
-        PP_CHECKLIST_BOOTSTRAP_PLUGIN_NAME,
-        PP_CHECKLIST_BOOTSTRAP_MIN_PARENT_VERSION
-    );
+	/**
+	 * Instantiate the PublishPress Add-on Framework. It is a simple framework
+	 * used to check PublishPress requirements. It is a dependency managed using
+	 * composer.
+	 */
+	$initializer = new PublishPressAddonFramework\Initializer(
+		PP_CHECKLIST_BOOTSTRAP_PLUGIN_NAME,
+		PP_CHECKLIST_BOOTSTRAP_MIN_PARENT_VERSION
+	);
 
-    // Check if PublishPress is installed and at the correct version.
-    if ($initializer->is_publishpress_installed()) {
+	// Check if PublishPress is installed and at the correct version.
+	if ( $initializer->is_publishpress_installed() ) {
 
-        // Define a constant to be a flag saying it is already loaded.
-        define('PP_CHECKLIST_BOOTSTRAP_LOADED', 1);
+		// Define a constant to be a flag saying it is already loaded.
+		define( 'PP_CHECKLIST_BOOTSTRAP_LOADED', 1 );
 
-        // Initialize the plugin's code.
-        $plugin = new PublishPress_Checklist_Bootstrap\Plugin;
-        $plugin->init();
-    }
+		// Initialize the plugin's code.
+		$plugin = new PublishPress_Checklist_Bootstrap\Plugin;
+		$plugin->init();
+	}
 }
